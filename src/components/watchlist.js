4babf0cf4843368d945d2bb5adbba8f113876222
Jinesh Patel <jinesh701@gmail.com>
2018-02-19 22:37:42 -0500

    added auto complete and button from material-ui

--- src/components/watchlist.js
+++ src/components/watchlist.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const buttonStyle = {
+  fontSize: 10,
+  height: 20,
+  margin: 5
+};
+
+const coins = [
+  'Bitcoin (BTC)',
+  'Litecoin (LTC)',
+  'Ethereum (ETH)',
+  'Waltonchain (WTC)',
+  'VeChain (VEN)',
+  'Bitcoin Cash (BCH)'
+];
 
 export class Watchlist extends React.Component {
   render() {
     return (
       <div>
-        <h1>Watchlist</h1>
+        <h2>Watchlist</h2>
+        <AutoComplete
+          floatingLabelText="Coin Name"
+          filter={AutoComplete.fuzzyFilter}
+          dataSource={coins}
+          maxSearchResults={5}
+        />
+        <RaisedButton
+          label="Add Coin"
+          style={buttonStyle}
+          labelStyle={buttonStyle}
+        />
       </div>
     );
   }
