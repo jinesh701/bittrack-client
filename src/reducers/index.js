5e346cc6a14932314f67059e9fba47275e71853c
Jinesh Patel <jinesh701@gmail.com>
2018-02-20 03:35:47 -0500

    added default reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
+import { ADD_COIN_TO_WATCHLIST } from '../actions';
+
 const initialState = {};
 
-export const reducer = (state = initialState, action) => {};
+export const reducer = (state = initialState, action) => {
+  return state;
+};
