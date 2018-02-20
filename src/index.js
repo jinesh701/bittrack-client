6a85e5ed59f5fe3e5d70939f6c3c9cbeac0ec669
Jinesh Patel <jinesh701@gmail.com>
2018-02-19 22:36:28 -0500

    added MuiThemeProvider for material-ui

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Watchlist from './components/watchlist';
 import store from './store';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Watchlist />
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <Watchlist />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
