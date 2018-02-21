1dad6ad6da965357c4a1c509f39d707b8f4b91a0
Jinesh Patel <jinesh701@gmail.com>
2018-02-21 08:35:57 -0500

    added store for smoke test to pass

--- src/components/watchlist.test.js
+++ src/components/watchlist.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import store from '../store';
 
 import Watchlist from './watchlist';
 
 describe('<Watchlist />', () => {
-  it('Renders without crashing', () => {
-    shallow(<Watchlist />);
+  it('Renders without crashing and has access to store', () => {
+    shallow(<Watchlist store={store} />);
   });
 });
