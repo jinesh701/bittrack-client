88dfa05e2aa528616109b10cb8f60a5e3c1cdff7
Jinesh Patel <jinesh701@gmail.com>
2018-02-22 08:01:45 -0500

    added thunk, promise, and logger

--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import promise from 'redux-promise';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer);
+export default createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk, promise, logger))
+);
