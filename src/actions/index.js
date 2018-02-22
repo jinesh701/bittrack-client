fdf4088b39732ce55a3811ebebed0b3b3b43189c
Jinesh Patel <jinesh701@gmail.com>
2018-02-22 08:02:34 -0500

    added fetchCoins and fetchCoinsSuccess actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,23 @@
+import topCoinsJson from '../topCoinsJson.json';
+
 export const ADD_COIN_TO_WATCHLIST = 'ADD_COIN_TO_WATCHLIST';
 export const addCoinToWatchlist = coin => ({
   type: ADD_COIN_TO_WATCHLIST,
   coin
 });
+
+export const FETCH_COIN_SUCCESS = 'FETCH_COIN_SUCCESS';
+export const fetchCoinSuccess = cryptoData => ({
+  type: FETCH_COIN_SUCCESS,
+  cryptoData
+});
+
+export const fetchCoins = coinName => dispatch => {
+  return Promise.resolve(
+    topCoinsJson.find(function(element) {
+      return element.id === coinName;
+    })
+  ).then(coin => {
+    dispatch(fetchCoinSuccess(coin));
+  });
+};
