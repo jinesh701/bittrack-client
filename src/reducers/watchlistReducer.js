2351c249ff2c71a5cd6b789f6c3837e90f6ca911
Jinesh Patel <jinesh701@gmail.com>
2018-02-22 21:17:10 -0500

    removed ADD_COIN_TO_WATCHLIST reducer

--- src/reducers/watchlistReducer.js
+++ src/reducers/watchlistReducer.js
@@ -4,9 +4,7 @@ const initialState = [];
 
 export default function watchlistReducer(state = initialState, action) {
   if (action.type === actions.FETCH_COIN_SUCCESS) {
-    return Object.assign([...state], action.cryptoData);
-  } else if (action.type === actions.ADD_COIN_TO_WATCHLIST) {
-    return [...state, Object.assign({}, action.coin)];
+    return [...state, Object.assign({}, action.cryptoData)];
   }
   return state;
 }
