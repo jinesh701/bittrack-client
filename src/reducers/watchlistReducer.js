dfc5c021b7a033477e8f3e79dea1e9405493a191
Jinesh Patel <jinesh701@gmail.com>
2018-03-06 00:14:28 -0500

    added reducer for FETCH_SAVED_COINS

--- src/reducers/watchlistReducer.js
+++ src/reducers/watchlistReducer.js
@@ -6,5 +6,8 @@ export default function watchlistReducer(state = initialState, action) {
   if (action.type === actions.FETCH_COIN_SUCCESS) {
     return [...state, Object.assign({}, action.cryptoData)];
   }
+  if (action.type === actions.FETCH_SAVED_COINS) {
+    return action.savedCoins;
+  }
   return state;
 }
