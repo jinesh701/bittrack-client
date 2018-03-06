123f7b5f5a7bf3f647f6c9f99f3fe096dd2424fc
Jinesh Patel <jinesh701@gmail.com>
2018-03-06 00:13:44 -0500

    added fetchSavedWatchlist to autopopulate users saved watchlist

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
-import topCoinsJson from '../topCoinsJson.json';
+export const FETCH_SAVED_COINS = 'FETCH_SAVED_COINS';
+export const fetchSavedCoins = savedCoins => ({
+  type: FETCH_SAVED_COINS,
+  savedCoins
+});
 
 export const FETCH_COIN_SUCCESS = 'FETCH_COIN_SUCCESS';
 export const fetchCoinSuccess = cryptoData => ({
@@ -6,13 +10,21 @@ export const fetchCoinSuccess = cryptoData => ({
   cryptoData
 });
 
+export const fetchSavedWatchlist = () => dispatch => {
+  return fetch('https://floating-harbor-45364.herokuapp.com/api/watchlist')
+    .then(res => res.json())
+    .then(watchlist => dispatch(fetchSavedCoins(watchlist)));
+};
+
 export const fetchCoins = coinName => dispatch => {
-  return Promise.resolve(
-    topCoinsJson.find(function(element) {
-      return element.id === coinName;
-    })
-  ).then(coin => {
-    console.log(coin);
-    dispatch(fetchCoinSuccess(coin));
-  });
+  return fetch(`https://api.coinmarketcap.com/v1/ticker/`)
+    .then(res => res.json())
+    .then(result =>
+      result.find(element => {
+        return element.id === coinName;
+      })
+    )
+    .then(coin => {
+      dispatch(fetchCoinSuccess(coin));
+    });
 };
