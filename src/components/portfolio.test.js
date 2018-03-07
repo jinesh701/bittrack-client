480045855e8d9f1dbd755cfb3e19686e0bd33f6b
Jinesh Patel <jinesh701@gmail.com>
2018-03-07 01:17:25 -0500

    added test for portfolio component

---
+++ src/components/portfolio.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import store from '../store';
+
+import Portfolio from './portfolio';
+
+describe('<Portfolio />', () => {
+  it('Renders without crashing and has access to store', () => {
+    shallow(<Portfolio store={store} />);
+  });
+
+  it('Has an array of portfolios as props', () => {
+    let portfolio = [];
+
+    let wrapper = shallow(<Portfolio store={store} portfolio={portfolio} />);
+    expect(wrapper.contains(portfolio)).toEqual(true);
+  });
+});
