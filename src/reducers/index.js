da6131d5b9fe7db1b47f90564dcd4a0293a1849b
Jinesh Patel <jinesh701@gmail.com>
2018-03-07 01:10:32 -0500

    added portfolio to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import portfolio from './portfolioReducer';
 import watchlist from './watchlistReducer';
 
 const rootReducer = combineReducers({
+  portfolio,
   watchlist
 });
 
