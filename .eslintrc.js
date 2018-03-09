7e03a1e30c078b9df14def9435224bd37efed153
Jinesh Patel <jinesh701@gmail.com>
2018-03-09 11:17:46 -0500

    added eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  env: {
+    jest: true,
+    browser: true
+  },
+  rules: {
+    'comma-dangle': 0,
+    'react/jsx-uses-vars': 1,
+    'react/display-name': 1,
+    'no-console': 1,
+    'arrow-parens': ['error', 'as-needed']
+  }
+};
