52bf9e9f38562674cd48316e4c937f328db6846a
Jinesh Patel <jinesh701@gmail.com>
2018-03-09 11:22:51 -0500

    fixed watchlist import

--- src/components/watchlist.test.js
+++ src/components/watchlist.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import store from '../store';
 
-import Watchlist from './watchlist';
+import { Watchlist } from './watchlist';
 
 describe('<Watchlist />', () => {
-  it('Renders without crashing and has access to store', () => {
-    shallow(<Watchlist store={store} />);
+  it('Renders without crashing', () => {
+    shallow(<Watchlist />);
   });
 });
