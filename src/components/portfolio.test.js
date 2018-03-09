781d78b7ee414c6f1ac7588cc78f2083d4030052
Jinesh Patel <jinesh701@gmail.com>
2018-03-09 11:21:11 -0500

    fixed import of portfolio

--- src/components/portfolio.test.js
+++ src/components/portfolio.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import store from '../store';
 
-import Portfolio from './portfolio';
+import { Portfolio } from './portfolio';
 
 describe('<Portfolio />', () => {
-  it('Renders without crashing and has access to store', () => {
-    const dispatch = jest.fn();
-    shallow(<Portfolio store={store} portfolio={{}} dispatch={dispatch}/>);
+  it('Renders without crashing', () => {
+    shallow(<Portfolio />);
   });
 });
