419fa45fec078003aa2542bd066ae3580563d274
Jinesh Patel <jinesh701@gmail.com>
2018-03-09 11:20:11 -0500

    fixed incorrect import

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import store from '../store';
 
-import App from './watchlist';
+import App from './app';
 import PortfolioCardExpandable from './portfolio-card';
 import WatchlistCardExpandable from './watchlist-card';
 
 describe('<App />', () => {
   it('Renders without crashing and has access to store', () => {
-    shallow(<App store={store} />);
+    shallow(<App />);
   });
 });
 
