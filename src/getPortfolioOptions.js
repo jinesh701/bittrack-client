0caf801cbbe73093d9b4b7593634542ccfa7784b
Jinesh Patel <jinesh701@gmail.com>
2018-03-12 23:30:00 -0400

    corrected function

--- src/getPortfolioOptions.js
+++ src/getPortfolioOptions.js
@@ -1,6 +1,6 @@
 const topCoinsJson = require('./topCoinsJson.json');
 
 export default function getPortfolioOptions(portfolio) {
-  const portfolioSymbols = portfolio.coinData.map(x => x.coinData.symbol);
+  const portfolioSymbols = portfolio.coinData.map(x => x.symbol);
   return topCoinsJson.filter(coin => portfolioSymbols.indexOf(coin.symbol) === -1);
 }
