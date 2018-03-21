37a4d24a484bb50671f1c927cbb282f7f36a7f36
Jinesh Patel <jinesh701@gmail.com>
2018-03-20 20:09:24 -0400

    added Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/app';
@@ -8,7 +9,9 @@ import store from './store';
 ReactDOM.render(
   <MuiThemeProvider>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </MuiThemeProvider>,
   document.getElementById('root')
