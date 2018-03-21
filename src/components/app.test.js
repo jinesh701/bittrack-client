69e256657272312cad37a01b2104216a3245e997
Jinesh Patel <jinesh701@gmail.com>
2018-03-21 00:25:22 -0400

    removed tests for extra components that were moved

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -2,23 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './app';
-import PortfolioCardExpandable from './portfolio-card';
-import WatchlistCardExpandable from './watchlist-card';
 
 describe('<App />', () => {
   it('Renders without crashing and has access to store', () => {
     shallow(<App />);
-  });
-});
-
-describe('<PortfolioCardExpandable />', () => {
-  it('Renders without crashing', () => {
-    shallow(<PortfolioCardExpandable />);
-  });
-});
-
-describe('<WatchlistCardExpandable />', () => {
-  it('Renders without crashing', () => {
-    shallow(<WatchlistCardExpandable />);
   });
 });
