6db72ac67eca8d1ff868830f9c6d77a8f03fb3e5
Jinesh Patel <jinesh701@gmail.com>
2018-03-21 00:24:51 -0400

    changed main app page to incorporate authentication

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import PortfolioCardExpandable from './portfolio-card';
-import WatchlistCardExpandable from './watchlist-card';
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
 
-export default function App() {
-  return (
-    <div>
-      <PortfolioCardExpandable />
-      <WatchlistCardExpandable />
-    </div>
-  );
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+
+import HeaderBar from './header-bar';
+import LandingPage from './landing-page';
+import Dashboard from './dashboard';
+import RegistrationPage from './registration-page';
+import LoginPage from './login-page';
+import { refreshAuthToken } from '../actions/auth';
+
+export class App extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn && !this.props.loggedIn) {
+      this.startPeriodicRefresh();
+    } else if (!nextProps.loggedIn && this.props.loggedIn) {
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      60 * 60 * 1000 // One hour
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+
+    clearInterval(this.refreshInterval);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <HeaderBar />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={RegistrationPage} />
+        <Route exact path="/login" component={LoginPage} />
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+});
+
+App.propTypes = {
+  dispatch: PropTypes.func,
+  loggedIn: PropTypes.bool
+};
+
+export default withRouter(connect(mapStateToProps)(App));
