b41261ab09155e111036b9fe295d14cf3afe6e78
Jinesh Patel <jinesh701@gmail.com>
2018-03-21 00:44:30 -0400

    added login-page component

---
+++ src/components/login-page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import LoginForm from './login-form';
+
+import './login-page.css';
+
+export function LoginPage(props) {
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
+  return (
+    <div className="home">
+      <h2 className="title">Login</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+LoginPage.propTypes = {
+  loggedIn: PropTypes.bool
+};
+
+export default connect(mapStateToProps)(LoginPage);
