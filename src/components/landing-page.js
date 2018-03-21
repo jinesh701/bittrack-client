ed8d7c624a137a4232e58c474b36e155b46e4cf9
Jinesh Patel <jinesh701@gmail.com>
2018-03-21 00:31:49 -0400

    added landing page component

---
+++ src/components/landing-page.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+export function LandingPage(props) {
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div className="home">
+      <h2>Welcome to Bittrack</h2>
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+LandingPage.propTypes = {
+  loggedIn: PropTypes.bool
+};
+
+export default connect(mapStateToProps)(LandingPage);
