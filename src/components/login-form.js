14da872007749d4b5d516d1502a83de3ce4e0e63
Jinesh Patel <jinesh701@gmail.com>
2018-03-23 12:25:56 -0400

    added demo user

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -34,6 +34,12 @@ export class LoginForm extends React.Component {
         className="login-form"
         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
       >
+        <p className="demo-header">Demo user</p>
+        <div className="demo-span">
+          <span className="demo">Username: demo, </span>
+          <span className="demo">Password: demodemo</span>
+        </div>
+
         {error}
         <div className="username-div">
           <Field
